364843227edb65d602d164619e8b6481f927b371
iulia sirbu <iuliasirbu47@gmail.com>
2022-05-07 16:55:46 +0300

    Initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Weather app listening on port ${port}!`);
+});
